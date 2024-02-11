4b9974727e0d5ca22115e90b0ac3f0f183f652ee
inkeaton <edo.vas@protonmail.com>
2024-02-11 11:38:18 +0100

    Started working on StarList and COnsList

--- public_html/constellation/consList.php
+++ public_html/constellation/consList.php
@@ -1,6 +1,6 @@
 <?php
-    include_once  dirname(__FILE__) . '/../_api/_utils/sessionControl.php';  
-    include_once  dirname(__FILE__) . '/../_api/_model/Constellation.php';     
+    include_once  dirname(__FILE__) . '/../_api/_model/Constellation.php';
+    session_start();    
 ?>
 
 <!DOCTYPE html>
@@ -8,93 +8,70 @@
     <head>
         <title>Lista delle constellazioni </title>
         <style>
-            body {background-color: rgb(84, 84, 250);}
-            #pageDiv {
+            .grid-container {
+                grid-template-columns: auto auto auto;
+                display: grid;
+                overflow: scroll; 
+            }
+            .grid-up-contain {
+                grid-template-rows: auto auto;
                 position: absolute;
-                width: 80%;
-                height:80%;
-                top: 10%;
-                left: 10%;
-                border: 2px;
-                border-radius: 2%;
-                border-color: black;
-                border-style: solid;
-                background-color: whitesmoke;
+                display: grid;
+                width: 90%;
+                height: 90%;
+                top: 5%;
+                left: 5%;
+                padding: 10px;
             }
-
-            #pageDiv > #inputDiv {
-                position:inherit;
-                width: 80%;
-                height: 20%;
-                left: 10%;
-            }
-            #inputDiv > input { width: 80%; }
-            #inputDiv > label, button { width: 10%; }
-           
-            #pageDiv > table {
-                position: inherit;
-                top: 10%;
-                width: 100%;
-            }
-
-            img {
-                width: 50px;
-                height: 50px;
-            }
-            
-            td {
-                width: 20%;
-                height: 20%;             
-                border: 0.5px;
-                border-color: black;
-                border-style: solid;
-            }
-            
-            td > div { 
-                height: 80%; 
+            .grid-item {
+                background-color: rgba(255, 255, 255, 0.8);
+                border: 1px;
+                padding: 20px;
+                font-size: 30px;
                 text-align: center;
             }
-
-            td > p { 
-                text-size-adjust: 10px; 
-                text-align: center;
+            img  {
+                width: 25%;
+                height: 25%;
             }
-
         </style>
     </head>
     <body>
         <!-- navbar -->
         <?php include  dirname(__FILE__) . "/../_modules/navbar.php"; ?>
-        <div id = "pageDiv"> 
-            <div id="inputDiv">
-                <label>Search Star: </label>
-                <input id="inputName" name="starName" type="text" value="" placeholder="search star...."> <br>
+        
+        <div class = "grid-up-contain"> 
+            <div class="grid-item">
+                <label>
+                    Search:<br>
+                    <input type="text" id="search"></input>
+                </label>
             </div>
-            <table id="constList">
-            </table>
+            <div class="grid-item grid-container" id="contain"></div>
         </div>
     </body>
+
     <script>
         const consList  = <?php  echo json_encode((new Constellation())->SelectAll()); ?>;
         displayCons(consList);
 
-        document.getElementById("inputName").value = "";
-        document.getElementById("inputName").addEventListener("keyup", changeSearch);
+        document.getElementById("search").value = "";
+        document.getElementById("search").addEventListener("keyup", changeSearch);
     
         function changeSearch(e) {
             var changeResult = [];
             var cI = 0;
             for(i=0;i<consList.length;i++)
-                if(consList[i].consName.search(document.getElementById("inputName").value) != -1) 
+                if(consList[i].consName.search(document.getElementById("search").value) != -1) 
                     changeResult[cI++] = consList[i];
             displayCons(changeResult);
         }
 
         function displayCons(list) {
-            outString = "<tr>";
+            var outString = "";
             index = 0;
             while(index<list.length && (list[index])) {
-                outString += "<td>" +
+                outString += "<div class='grid-item'>" +
                                "<div>" +
                                     "<img src='../_resources/img/starImg.png'>" +
                                 "</div>" +
@@ -104,13 +81,10 @@
                                         list[index++].consName + 
                                     "</a>" +
                                 "</p>" +
-                            "</td>";
-                if(index%5 == 0) outString += "</tr><tr>";    
+                            "</div>";
             }
-            if(index%5!=0 || index == 0) outString += "</tr>";
-            document.getElementById("constList").innerHTML = outString;
+            document.getElementById("contain").innerHTML = outString;
         }
         
-
     </script>
 </html>
--- public_html/stars/starList.php
+++ public_html/stars/starList.php
@@ -1,119 +1,90 @@
-<!DOCTYPE html>
 <?php
-    include_once dirname(__FILE__) . '/../_api/_utils/sessionControl.php'; 
     include_once  dirname(__FILE__) . '/../_api/_model/Star.php';  
+    session_start();  
 ?>
-<html lang="it">
+
+<!DOCTYPE html>
+<html>
     <head>
-        <title>starList</title>
+        <title>Lista delle Stelle </title>
         <style>
-            body {background-color: rgb(84, 84, 250);}
-            #pageDiv {
+            .grid-container {
+                grid-template-columns: auto auto auto;
+                display: grid;
+                overflow: scroll; 
+            }
+            .grid-up-contain {
+                grid-template-rows: auto auto;
                 position: absolute;
-                width: 80%;
-                height:80%;
-                top: 10%;
-                left: 10%;
-                border: 2px;
-                border-radius: 2%;
-                border-color: black;
-                border-style: solid;
-                background-color: whitesmoke;
+                display: grid;
+                width: 90%;
+                height: 90%;
+                top: 5%;
+                left: 5%;
+                padding: 10px;
             }
-
-            #pageDiv > #inputDiv {
-                position:inherit;
-                width: 80%;
-                height: 20%;
-                left: 10%;
-            }
-            #inputDiv > input { width: 80%; }
-            #inputDiv > label, button { width: 10%; }
-           
-            #pageDiv > table {
-                position: inherit;
-                top: 10%;
-                width: 100%;
-            }
-
-            img {
-                width: 50px;
-                height: 50px;
-            }
-            
-            td {
-                width: 20%;
-                height: 20%;             
-                border: 0.5px;
-                border-color: black;
-                border-style: solid;
-            }
-            
-            td > div { 
-                height: 80%; 
+            .grid-item {
+                background-color: rgba(255, 255, 255, 0.8);
+                border: 1px;
+                padding: 20px;
+                font-size: 30px;
                 text-align: center;
             }
-
-            td > p { 
-                text-size-adjust: 10px; 
-                text-align: center;
+            img  {
+                width: 25%;
+                height: 25%;
             }
-
         </style>
     </head>
     <body>
-        <?php
-            // navigation bar
-            include  dirname(__FILE__) .  "/../_modules/navbar.php"; 
-        ?>
-        <div id="pageDiv">
-            <div id="inputDiv">
-                <label>Search Star: </label>
-                <input id="inputName" name="starName" type="text" value="" placeholder="search star...."> <br>
+        <!-- navbar -->
+        <?php include  dirname(__FILE__) . "/../_modules/navbar.php"; ?>
+        
+        <div class = "grid-up-contain"> 
+            <div class="grid-item">
+                <label>
+                    Search:<br>
+                    <input type="text" id="search"></input>
+                </label>
             </div>
-            <table id="result">
-            
-            </table>
-        </div>       
-        <!-- footer -->
-        <?php include  dirname(__FILE__) . "/../_modules/footer.html"; ?> 
+            <div class="grid-item grid-container" id="contain"></div>
+        </div>
     </body>
+
     <script>
-        const starList = <?php echo json_encode((new Star())->SelectAll());?>;
+        const starList  = <?php  echo json_encode((new Star())->SelectAll()); ?>;
         displayStar(starList);
 
-        document.getElementById("inputName").value = "";
-        document.getElementById("inputName").addEventListener("keyup", changeSearch);
+        document.getElementById("search").value = "";
+        document.getElementById("search").addEventListener("keyup", changeSearch);
     
         function changeSearch(e) {
             var changeResult = [];
             var cI = 0;
             for(i=0;i<starList.length;i++)
-                if(starList[i].starName.search(document.getElementById("inputName").value) != -1) 
+                if(starList[i].StarName.search(document.getElementById("search").value) != -1) 
                     changeResult[cI++] = starList[i];
             displayStar(changeResult);
         }
 
         function displayStar(list) {
-            outString = "<tr>";
+            var outString = "";
             index = 0;
             while(index<list.length && (list[index])) {
-                outString += "<td>" +
+                outString += "<div class='grid-item'>" +
                                "<div>" +
                                     "<img src='../_resources/img/starImg.png'>" +
                                 "</div>" +
                                 "<hr>" +
                                 "<p>" +
-                                    "<a href='starDetails.php?starID="+list[index].starID +"'>" + 
+                                    "<a href='StarDetails.php?starID="+list[index].starID +"'>" + 
                                         list[index++].starName + 
                                     "</a>" +
                                 "</p>" +
-                            "</td>";
-                if(index%5 == 0) outString += "</tr><tr>";    
+                            "</div>";
             }
-            if(index%5!=0 || index == 0) outString += "</tr>";
-            document.getElementById("result").innerHTML = outString;
+            document.getElementById("contain").innerHTML = outString;
         }
+        
     </script>
-
 </html>
