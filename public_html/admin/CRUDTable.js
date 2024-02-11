dbd7650963468ca7b65667bcce8145fe9e8a3dba
Neorya <51713192+Neorya@users.noreply.github.com>
2024-02-12 00:43:19 +0100

    Update CRUDTable.js

--- public_html/admin/CRUDTable.js
+++ public_html/admin/CRUDTable.js
@@ -1,11 +1,5 @@
 class CRUDTable {
-    #pageStyle= "#CRUDdiv { position: absolute; left: 0%; width: 100%; height: 90%; top: 10%; }" + 
-                "#CRUDdiv > #CRUD {position: inherit; left: 1%; overflow: scroll; width: 98%; height: 90%; border: 1px; border-color: black; border-style: solid;}" +
-                "#CRUD > table {  width: 100%;}" +
-                "#CRUD > button { position:relative; width: 80%; left : 10%; background-color: #0096FF; border: solid; border-color: black;}" +
-                "#CRUD > button:hover {  background-color: #00FFFF; }" +
-                "td { border: 2px; height: 10%; border-color: black; border-style: solid;}";   
-    
+
     #h1Element = "Basic h1";    
     static insUrl = "";
     static updUrl = "";
@@ -13,7 +7,6 @@ class CRUDTable {
     static id = 0;
 
     set h1Element(newH1)    { this.#h1Element = newH1; }
-    set pageStyle(newStyle) { this.#pageStyle = newStyle; }
 
     constructor(list) {
         CRUDTable.id = 0;
@@ -23,27 +16,31 @@ class CRUDTable {
     }
 
     SelectRow(id, element)  { throw new Error('The child class must have genRow method!!'); }
-    InsertRow(id)           { throw new Error('The child class must have Insert Row!!');}    
+    InsertRow(id)           { throw new Error('The child class must have Insert Row!!');    }    
     init()                  { throw new Error('The child class must have init method!!'); }
    
     #buildPage(callClass) {
         var div = document.createElement("div");
-        var style = document.createElement("style");
+        var link = document.createElement("link");
         var h1 = document.createElement("h1");
         var childDiv = document.createElement("div");
         var table = document.createElement("table");
         var button = document.createElement("button");
+        
+        div.classList.add("mainDiv");
         div.id = "CRUDdiv";
         childDiv.id = "CRUD";
+        childDiv.classList.add("crud");
         table.id = "CRUDtable";
         button.innerHTML = "Inserisci un nuovo elemento";
         button.id = "insertButton";
         h1.innerHTML = this.#h1Element;
-        style.innerText = this.#pageStyle;
+        link.href = '../CRUD.css';
+        link.rel = 'stylesheet';
+
         button.onclick = function() { CRUDTable.ButtonInsert(callClass); };
-        document.head.appendChild(style);
+        document.head.appendChild(link);
         document.body.appendChild(div);
-        document.head.style = this.#pageStyle;
         div.appendChild(h1);
         div.appendChild(childDiv);
         childDiv.appendChild(button);
