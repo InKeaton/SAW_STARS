623c5a28fb32c9463d356c6bb2a863dbb2864c42
Neorya <51713192+Neorya@users.noreply.github.com>
2024-02-13 17:03:10 +0100

    Update Review.js

--- public_html/admin/review/Review.js
+++ public_html/admin/review/Review.js
@@ -22,6 +22,7 @@ class Review extends CRUDTable {
     static SelectValue(obj, val) {
         obj[val] = obj[val].toString().replace("unselected", "selected");
         var ret = Review.StringSelect(obj);
+        obj[val] = obj[val].toString().replace("selected", "unselected");
         return ret;
     }
 
