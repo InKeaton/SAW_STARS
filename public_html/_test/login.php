85971858f401ff40bba7c265a0cc1fee54f2b3d4
Neorya <51713192+Neorya@users.noreply.github.com>
2024-02-13 15:27:45 +0100

    Test dir and file

---
+++ public_html/_test/login.php
@@ -0,0 +1,3 @@
+<?php
+    include_once dirname(__FILE__) . "/../_api/auth_api/loginApi.php";
+?>
---
+++ public_html/_test/update_profile.php
@@ -0,0 +1,3 @@
+<?php
+    include_once dirname(__FILE__) . "/../_api/user_api/updateUser.php";
+?>
