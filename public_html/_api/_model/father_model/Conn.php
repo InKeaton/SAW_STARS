ddae238a2f75a3ac311277360aed1764ee587ac6
Neorya <51713192+Neorya@users.noreply.github.com>
2024-02-15 01:56:50 +0100

    Migliorato il destructor

--- public_html/_api/_model/father_model/Conn.php
+++ public_html/_api/_model/father_model/Conn.php
@@ -2,12 +2,12 @@
 	class Conn {
 		private $conn;
 		function __construct() {
-			$this->conn = new mysqli('localhost', 'root', '', 'galileoDb');
+			$this->conn = new mysqli('localhost', 'S4965918', 'CEFALOPODE02!', 'S4965918');
 			if($this->conn->connect_errno) 
 				die(json_encode(array('status' => 0, 'message' => 'Connection: ' . $this->conn->connect_error)));
 		}
 		
-		function __destruct() { $this->conn->close(); }
+		function __destruct() { $this->conn->close(); unset($this->conn);}
 		public function GetConn() { return $this->conn; }	
 	}
 ?>
