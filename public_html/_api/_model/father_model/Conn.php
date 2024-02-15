0583ce637a39ccc7709790d0d2fd132e100e92bc
Neorya <51713192+Neorya@users.noreply.github.com>
2024-02-15 02:04:43 +0100

    Update Conn.php

--- public_html/_api/_model/father_model/Conn.php
+++ public_html/_api/_model/father_model/Conn.php
@@ -2,7 +2,7 @@
 	class Conn {
 		private $conn;
 		function __construct() {
-			$this->conn = new mysqli('localhost', 'S4965918', 'CEFALOPODE02!', 'S4965918');
+			$this->conn = new mysqli('localhost', 'root', '', 'galileoDb');
 			if($this->conn->connect_errno) 
 				die(json_encode(array('status' => 0, 'message' => 'Connection: ' . $this->conn->connect_error)));
 		}
